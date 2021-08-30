f3962700d53b88f9681ee38572ff4ec8d82271e8
LGVN212351\Test <15520659@gm.uit.edu.vn>
2021-08-30 22:11:53 +0700

    Create layout footer

--- react-bookingticket-movie/src/services/QuanLyRapService.js
+++ react-bookingticket-movie/src/services/QuanLyRapService.js
@@ -11,6 +11,17 @@ export class QuanLyRapService extends baseService {
     layDanhSachHeThongRap = () => {
         return this.get(`/api/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=${GROUPID}`);
     }
+
+    layThongTinLichChieuPhim = (maPhim) => {
+        return this.get(`/api/QuanLyRap/LayThongTinLichChieuPhim?maPhim=${maPhim}`)
+    }
+    
+    layThongTinHeThongRap = () => {
+        return this.get(`/api/QuanLyRap/LayThongTinHeThongRap`);
+    }
+    layThongTinCumRap = (maHeThongRap) => {
+        return this.get(`/api/QuanLyRap/LayThongTinCumRapTheoHeThong?maHeThongRap=${maHeThongRap}`);
+    }
 }
 
 export const quanLyRapService = new QuanLyRapService();
