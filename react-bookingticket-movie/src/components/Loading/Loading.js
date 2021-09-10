0558b76356643eafb2b6f66f30f4943b81893cf1
LGVN212351\Test <15520659@gm.uit.edu.vn>
2021-09-10 22:54:26 +0700

    Add Layout Profile and Modify Component Loading

--- react-bookingticket-movie/src/components/Loading/Loading.js
+++ react-bookingticket-movie/src/components/Loading/Loading.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import './Loading.css';
 
 
 export default function Loading(props) {
@@ -11,7 +12,8 @@ export default function Loading(props) {
         <Fragment>
             {isLoading ?
                 <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: 'rgba(0,0,0,.5)', display: 'flex', justifyContent: 'center', alignItems: 'center', zIndex: 99 }}>
-                    <div className="text-4xl text-white">Loading ...</div>
+                    <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12 mb-4"></div>
+                    <div className="text-center text-white text-4xl font-semibold">Loading ...</div>
                 </div> : ''
 
             }
